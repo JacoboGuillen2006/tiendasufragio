4f5df6a44baa625185ca3fe74967baf62fe69dee
Jacobo Guillen 2006.com <jacoboguillen79.com>
2022-05-03 16:12:48 -0500

    se guarda el index

---
+++ firebase.js
@@ -0,0 +1,24 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmMJjxhE2THTq84S3XKuO_eHrM9686lig",
+    authDomain: "latiendita-1db59.firebaseapp.com",
+    projectId: "latiendita-1db59",
+    storageBucket: "latiendita-1db59.appspot.com",
+    messagingSenderId: "683687376753",
+    appId: "1:683687376753:web:239599fecb858a6221a8d0",
+    measurementId: "G-ZLG2LHEN78"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  */
