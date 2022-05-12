1d7c21ef10c6bcec5b0ea4818a8445b90262a990
Your Name <you@example.com>
2022-05-12 17:25:28 -0500

    ok

--- controlador.js
+++ controlador.js
@@ -1,108 +1,244 @@
 //OBJETOS
 let producto1={
     nombre:"Aji-no-men",
-    precio:500000,
+    precio:2000,
     estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Aji-no-men%20Pollo%20con%20verduras.jfif?alt=media&token=96b210a8-0eea-479f-a4a6-c6ec8a31347b"
 }
 
 let producto2={
     nombre:"Camisa negra de the owl house grom versión",
-    precio:500000,
+    precio:59000,
     estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camisa%20negra%20de%20The%20owl%20house%20grom%20versi%C3%B3n.png?alt=media&token=cdab9b7c-b2e1-483c-a808-052146f3929b"
 }
 
 let producto3={
     nombre:"Camiseta del Real Madrid edición el clasico",
-    precio:500000,
-    estado:"Nuevo",
+    precio:85000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camiseta%20de%20Real%20Madrid%20el%20Clasico%20Edition.jpg?alt=media&token=9dded7a3-b14e-494e-92ba-e00757ab8a9e"
 }
 
 let producto4={
     nombre:"Funko pop de doctor strange supremo what if",
-    precio:500000,
-    estado:"Nuevo",
+    precio:50000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Funko%20Pop%20de%20Doctor%20strange%20Supremo.jpg?alt=media&token=2269fd40-f185-484b-bc6f-21ff7af236f4"
 }
 
 let producto5={
     nombre:"Funko pop de iron man con el guantelete de avengers end game",
-    precio:500000,
-    estado:"Nuevo",
+    precio:45000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Funko%20pop%20Iron%20man%20End%20game%20con%20el%20guantelete.jpg?alt=media&token=bb7cb5e8-47e6-435b-9e64-111fa0169142"
 }
 
 let producto6={
     nombre:"Funko pop de spider man no way home",
-    precio:500000,
-    estado:"Nuevo",
+    precio:50000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Funko%20pop%20de%20Spider%20Man.jpg?alt=media&token=a1b7baa5-f9fd-44db-9d59-808bc61b93ba"
 }
 
 let producto7={
     nombre:"Galletas oreo cubiertas",
-    precio:500000,
-    estado:"Nuevo",
+    precio:4000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Galleta%20Oreo%20Cubierta.jfif?alt=media&token=7e8ae6af-c49c-4465-8956-504907f38662"
 }
 
 let producto8={
     nombre:"Play station 5 perzonalizado de spider man",
-    precio:500000,
-    estado:"Nuevo",
+    precio:3000000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Play%20station%205%20Perzonalizado%20(spider%20Man).jpg?alt=media&token=b53b76b7-9d18-4c64-b47e-2a76e9953bc5"
 }
 
 let producto9={
     nombre:"Sueter azul de the owl house lumity grom versión",
-    precio:500000,
-    estado:"Nuevo",
+    precio:75000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Sueter%20azul%20de%20the%20owl%20house%20lumity%20grom%20versi%C3%B3n.png?alt=media&token=e93bb860-ec24-411e-81fe-08cb11677532"
 }
 
 let producto10={
     nombre:"Sueter gris de The owl house hooty versión",
-    precio:500000,
-    estado:"Nuevo",
+    precio:75000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Sueter%20gris%20de%20The%20owl%20house%20hooty%20versi%C3%B3n.jfif?alt=media&token=dc342bfc-c4d7-445c-b747-6f1ab25bf05a"
 }
 
 let producto11={
     nombre:"camiseta gris de the owl house lumity grom versión",
-    precio:500000,
-    estado:"Nuevo",
+    precio:59000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/camiseta%20gris%20de%20the%20owl%20house%20lumity%20grom%20versi%C3%B3n.jpg?alt=media&token=7ab7efec-080c-4550-b1c5-ead0f9a7882e"
 }
 
 let producto12={
     nombre:"Camiseta rodolfo Auronplay",
-    precio:500000,
-    estado:"Nuevo",
+    precio:60000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/camiseta%20manga%20larga%20de%20rodolfo%20auronplay.jpg?alt=media&token=caaf43e2-d8ee-4350-abea-21dd0683fc0c"
 }
 
 let producto13={
     nombre:"Conjunto para hombre de blazer con sueter negro",
-    precio:500000,
-    estado:"Nuevo",
+    precio:90000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/blazer-negro-con-jersey-cuello-alto.jpg?alt=media&token=96ef658d-a21e-44f1-a059-ba9ae6dd80b7"
 }
 
 let producto14={
     nombre:"Nike Mercurial Superfly Dragonfly",
-    precio:500000,
-    estado:"Nuevo",
+    precio:120000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Nike%20Mercurial%20superfly%20Dragonfly.jpg?alt=media&token=11e23363-4fb9-4154-9cf3-345fdebed529"
 }
 
 let producto15={
     nombre:"Nike Mercurial CR7 Chapter 5",
-    precio:500000,
-    estado:"Nuevo",
+    precio:90000,
+    estado:true,
+    popularidad:5,
     foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Nike%20Mercurial%20CR7%20Chapter%205.jpg?alt=media&token=296652ab-71a2-4146-b204-161520f1c33b"
 }
+
+let producto16={
+    nombre:"Xbox series X",
+    precio:2500000,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/xbox%20series%20x.jpg?alt=media&token=e32caf40-16d8-40af-9fa9-221918a71cfc"
+}
+
+let producto17={
+    nombre:"BALÓN AL RIHLA LEAGUE BOX",
+    precio:159990,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Balon%20Al%20Rihla.jpg?alt=media&token=6276fc5a-37b7-4752-8e2f-530c24dede10"
+}
+
+let producto18={
+    nombre:"Camiseta de Dragon Ball Z negra",
+    precio:37000,
+    estado:true,
+    popularidad:4,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camiseta%20de%20dragon%20ball%20negra.jpg?alt=media&token=00092b89-0686-4ac9-9275-09b3cba88785"
+}
+
+let producto19={
+    nombre:"Camiseta de Dragon Ball Z",
+    precio:37000,
+    estado:true,
+    popularidad:4,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camiseta%20de%20dragon%20ball.jpg?alt=media&token=e68e88dc-5fd0-4b3e-860d-18920d508664"
+}
+
+let producto20={
+    nombre:"Camiseta de Goku Ultrainstinto",
+    precio:42000,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camiseta%20de%20Goku%20Ultrainstinto%20Kamehameha.jpg?alt=media&token=ffd45965-0035-4395-a8ae-98b310adc32d"
+}
+
+let producto21={
+    nombre:"Camiseta de Doctor Strange",
+    precio:42000,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Camiseta%20de%20Doctor%20Strange.jpg?alt=media&token=d68003ee-9da0-4f28-9011-58c42c47ec25"
+}
+
+let producto22={
+    nombre:"Chaqueta de Doctor Strange",
+    precio:50000,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Chaqueta%20de%20Doctor%20Strange.jpg?alt=media&token=917f4cc6-6f2c-43b1-a0dc-13a2d45da673"
+}
+
+let producto23={
+    nombre:"Chaqueta de Docotr Strange azul",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Chaqueta%20de%20Doctor%20Strange%20azul.jpg?alt=media&token=a9a22dfa-2306-4b1d-a58a-9965e74e30b6"
+}
+
+let producto24={
+    nombre:"Collar replica del ojo de agamoto",
+    precio:89000,
+    estado:true,
+    popularidad:5,
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-1db59.appspot.com/o/Collar%20de%20replica%20del%20ojo%20de%20agamoto.webp?alt=media&token=3c6c2c0e-e936-4c5b-b2a0-0b8090e84664"
+}
+
+let producto25={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
+let producto26={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
+let producto27={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
+let producto28={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
+let producto29={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
+let producto30={
+    nombre:"",
+    precio:1,
+    estado:true,
+    popularidad:5,
+    foto:""
+}
+
 
 //ARREGLO
 let productos=Array(
@@ -120,7 +256,23 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+
 )
 
 //RECORRO EL ARREGLO
@@ -137,7 +289,34 @@ productos.forEach(function(producto){
 
     let tarjeta = document.createElement ("div")
     tarjeta.classList.add ("card")
+
+    let nombreproducto = document.createElement ("h3")
+    nombreproducto.classList.add("text-center")
+    nombreproducto.textContent = producto.nombre
+
+    let precioproducto = document.createElement ("h5")
+    precioproducto.classList.add("text-center","text-success")
+    precioproducto.textContent = producto.precio
+
+    let estadoproducto = document.createElement ("h5")
+    estadoproducto.classList.add("text-center")
+    estadoproducto.textContent = producto.estado
+
+    let popularidadproducto = document.createElement ("h5")
+    popularidadproducto.classList.add("text-center")
+    popularidadproducto.textContent = producto.popularidad
+
+
+
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreproducto)
+    tarjeta.appendChild(precioproducto)
+    tarjeta.appendChild(estadoproducto)
+    tarjeta.appendChild(popularidadproducto)
+
+
+
+
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
 
